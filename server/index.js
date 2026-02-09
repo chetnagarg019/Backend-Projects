b0ce3f933633bb8f3aa1994ccea5c1d7280e696e
chetna <chetnagarg019@gmail.com>
2026-02-10 01:24:32 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+// import mongoose, { connect } from 'mongoose';
+import connectDB from "./config/db.js";
+// import authRoutes from "./routes/authRoutes.js";
 
+
+dotenv.config();
+connectDB();
 const app = express();
+
+app.use(cors());
 app.use(express.json()); // ye ek middleware hai jo re.body ko json format me convert krta h
 
 
@@ -8,59 +18,12 @@ app.get("/", (req, res) => {
     res.send("Server is running hello");
 });
 
-const notes = [];
-
-app.post("/notes",(req,res) => {
-   notes.push(req.body);
-
-   res.status(201).json({
-     message : "note created successfully"
-   })
-    
-})
-
-//server se data fetch krne ke liye 
-app.get("/notes",(req,res) => {
-    res.status(200).json({
-        message : 'note fetched succesfully',
-        notes : notes
-    })
-})
-
-//delete krne ke liye note
-app.delete("/notes/:index",(req,res) => {
-
-    const index = req.params.index;
-    // delete notes [index] => null rerurn
-    notes.splice(index, 1)
-    res.status(200).json({
-        message : "note deleted successfully"
-    })
-})
-
-// : colon likhne se hota kya hai ki express ko pta chlta jata hai ki ye chiz tumahri dynamic rhegi esliye colon ka use kiya jata hai 
-//jo bhi note humene delet kiya hai uski jagah null likha hua aa jata hai 
-
-//update krne ke liye 
-app.patch("/notes/:index",(req,res) => {
-
-    const index = req.params.index;
-    const description = req.body.description
-
-    notes[index].description = description;
-    res.status(200).json({
-        message : "note updated successfully"
-    })
-})
-
 
 
 
 app.listen(5000, () => {
-    console.log("Server started on port 3000");
+    console.log("Server started on port 5000");
 });
-//jo bhi data body ke andr aane vala hai usko express read nhi kr skta 
-//line4 middleware express ke andr vo capcity deta hai jisse vo body ke andr aane vale data ko read kr ske
 
 //production level pr jb backend ko steup krte h to 
 //folder ke andr src name ka folder create krte hai uske andr app.js file
